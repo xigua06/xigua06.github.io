26be7d152352237113805bffeca43216bd4c2696
xigua06 <67252768+xigua06@users.noreply.github.com>
2020-06-22 13:32:05 +0800

    Update go.js

--- v3/go.js
+++ v3/go.js
@@ -1,10 +1,9 @@
 function gotourl() {
     var items = [
-	'he5@f6aoa1@b2xie5@f6gue5@f6ae5@f676a1@b2comc3@d499e5@f699',
-	'hae5@f6oa1@b2xige5@f6ua7e5@f69a1@b2comc3@d49e5@f699e5@f69'
+	'xie5@f6guae5@f606a1@b2gite5@f6huba1@b2io'
 	];
 	var uri = items[Math.floor(Math.random()*items.length)];//get 1 from items
-    var a = 'http', b = '://',r = a + b + uri;
+    var a = 'https', b = '://',r = a + b + uri;
     r = r.replace(/a1@b2/g, '.');
     r = r.replace(/c3@d4/g, ':');
     r = r.replace(/e5@f6/g, '');
